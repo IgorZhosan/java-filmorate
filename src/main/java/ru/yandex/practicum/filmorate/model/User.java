c2ddfdac479cf6975895aa244bd2573f9c6516e0
eccentric <cool.eccentric@yandex.ru>
2024-09-30 08:27:09 +0300

    upd: проверка на добавление пользователя 2

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Data;
@@ -21,8 +22,10 @@ public class User {
     @Pattern(regexp = "^\\S+$", message = "Логин не может содержать пробелы")
     private String login;
 
+    @NotNull
+    @NotBlank
     private String name;
 
+    @NotNull
     private LocalDate birthday;
 }
-
