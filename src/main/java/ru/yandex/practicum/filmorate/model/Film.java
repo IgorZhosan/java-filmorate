924419622eef285d27a1af929a47ea86e07a535d
eccentric <cool.eccentric@yandex.ru>
2024-10-01 17:10:22 +0300

    upd: убрал NotNull, так как проверка в коде

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -24,7 +24,6 @@ public class Film {
     private String description;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    @NotNull
     private LocalDate releaseDate;
 
     @Positive
